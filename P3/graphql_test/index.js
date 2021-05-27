38be4ca644b93a8b3759546274228a72c328a26b
Hassanain Ali <hassanainali@Hassanains-MacBook-Pro-16.local>
2021-05-27 14:28:43 +0200

    tilføjet cors til graphql

--- P3/graphql_test/index.js
+++ P3/graphql_test/index.js
@@ -1,12 +1,14 @@
-import express from 'express';
+import express from "express";
 //import dotenv from "dotenv";
-require('dotenv').config({ path: __dirname + '/.env' })
+require("dotenv").config({ path: __dirname + "/.env" });
 //dotenv.config();
-console.log("CONNECTION-", process.env.CONNECTION)
-import { graphqlHTTP } from 'express-graphql';
-import { schema } from './data/schema';
+console.log("CONNECTION-", process.env.CONNECTION);
+import { graphqlHTTP } from "express-graphql";
+import { schema } from "./data/schema";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Graphql is amazing!");
